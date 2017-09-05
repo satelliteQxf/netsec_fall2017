005c8bab53ce55b281c2b6fa502ed0ba71ee5f89
satelliteQxf <475313412@qq.com>
2017-09-05 12:23:44 -0400

    hw lab1b

--- lab_1b/submission.py
+++ lab_1b/submission.py
@@ -1,25 +1,68 @@
 from playground.network.packet import PacketType
 from playground.network.packet.fieldtypes import UINT32,STRING,BUFFER
+import random
+'''
+protocol:
+1. The client sneds a request to the server to start a conversation:
+* client sends Request(ID) to server
 
-class MyPacket(PacketType):
-	DEFINITION_IDENTIFIER = "lab1b.student_qxf.MyPacket"
+2. The server responds by sending a key and the id of server:
+* server sends verify(ID,key) to client
+
+3. The client sends a confirm text to start the conversation:
+* client sends confirm(ID,SERVER_ID,KEY) to client
+
+The key here is to make sure this conversation is made by this specific client and server.
+'''
+
+class Request(PacketType):
+	DEFINITION_IDENTIFIER = "lab1b.student_qxf.request"
 	DEFINITION_VERSION = "1.0"
 	FIELDS = [
-		("counter1",UINT32),
-		("counter2",UINT32),
-		("name",STRING),
-		("data",BUFFER)
+		("ID",UINT32),
 	]
 
+class Verify(PacketType):
+	DEFINITION_IDENTIFIER = "lab1b.student_qxf.verify"
+	DEFINITION_VERSION = "1.0"
+	FIELDS = [
+		("ID",UINT32),
+		("key",UINT32)
+	]
 
-packet1 = MyPacket()
-packet1.counter1 = 100
-packet1.counter2 = 200
-packet1.name = "qxf"
-packet1.data = b"This may look like a string but it's actually a sequence of bytes."
-packetBytes = packet1.__serialize__()
+class Confirm(PacketType):
+	DEFINITION_IDENTIFIER = "lab1b.student_qxf.confirm"
+	DEFINITION_VERSION = "1.0"
+	FIELDS = [
+		("Client_ID",UINT32),
+		("Server_ID",UINT32),
+		("key",UINT32)
+	]
 
-packet2 = PacketType.Deserialize(packetBytes)
+def BasicUnitTest():
+	packet1 = Request()
+	packet1.ID = 1
+	packet1Bytes = packet1.__serialize__()
+	packet1a = Request.Deserialize(packet1Bytes)
+	assert packet1 == packet1a
+	assert packet1.ID == packet1a.ID
 
-if(packet1 == packet2):
-	print("These two packets are the same!")
+	packet2 = Verify()
+	packet2.ID = 2
+	packet2.key = random.randint(1,100)
+	packet2Bytes = packet2.__serialize__()
+	packet2a = Verify.Deserialize(packet2Bytes)
+	assert packet2 == packet2a
+
+	packet3 = Confirm()
+	packet3.Client_ID = 1
+	packet3.Server_ID = 2
+	packet3.key = packet2.key
+	packet3Bytes = packet3.__serialize__()
+	packet3a = Confirm.Deserialize(packet3Bytes)
+	assert packet3 == packet3a
+
+if __name__=="__main__":
+	BasicUnitTest()
+
+
